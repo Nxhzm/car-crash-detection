a72b03c596a5e25bd845f5b869f45682253b2d5f
Nahum Solomon <76616055+Nxhzm@users.noreply.github.com>
2020-12-26 18:15:49 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var car, wall;
+var speed, weight;
+var deformation; 
+
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=random(400,1500);
+  car = createSprite(50, 200, 50, 50);
+  car.shapeColour = color (80,70,80);
+  wall = createSprite(1550,200,60,height/2);
+  wall.shapeColor = color (80,80,80);
+  
+  car.velocityX = speed;
+}
+
+function draw() {
+  background(0);  
+  //background(255,255,255);  
+  drawSprites();
+ 
+  deformation = (0.5 * weight * speed * speed) / 22500;
+
+
+ if (car.x - wall.x < wall.width/2 + car.width/2 && wall.x - car.x < wall.width/2 + car.width/2 )  {
+    
+    car.velocityX = 0;
+
+    if (deformation < 100) {
+      car.shapeColor = color (0,225, 0);
+    }
+  
+    else if ((deformation > 100) && (deformation < 180)) {
+      car.shapeColor = color (230,230, 0);
+    }
+  
+    else if (deformation > 180) {
+      car.shapeColor = color (255,0, 0);
+    }
+  
+    else car.shapeColour = color (80,70,80);
+  }
+}
